cabdb2f9ab24a66764468ef880a77bc2ffb5e4df
itswilliam7 <104334032+itswilliam7@users.noreply.github.com>
2022-06-22 20:39:47 -0400

    upload important files
    first version, works but needs work

---
+++ index.js
@@ -0,0 +1,82 @@
+var background = document.getElementById("background");
+var skin = document.getElementById("skin");
+var eyes = document.getElementById("eyes");
+var eyebrows = document.getElementById("eyebrows");
+var mouth = document.getElementById("mouth");
+var faceitems = document.getElementById("faceitems");
+var glasses = document.getElementById("glasses");
+var head = document.getElementById("head");
+var shirt = document.getElementById("shirt");
+
+
+// backgroundDropdown.onchange = function(event){
+//    alert("Your background is " + backgroundDropdown.value + "!");    
+// }
+
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+let imageObj = new Image();
+
+background.addEventListener('change', function () {
+  imageObj.src = this.options[this.selectedIndex].dataset.img;
+  imageObj.onload = function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+  };
+});
+
+skin.addEventListener('change', function () {
+  imageObj.src = this.options[this.selectedIndex].dataset.img;
+  imageObj.onload = function() {
+    context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+  };
+});
+
+eyes.addEventListener('change', function () {
+  imageObj.src = this.options[this.selectedIndex].dataset.img;
+  imageObj.onload = function() {
+    context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+  };
+});
+
+eyebrows.addEventListener('change', function () {
+  imageObj.src = this.options[this.selectedIndex].dataset.img;
+  imageObj.onload = function() {
+    context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+  };
+});
+
+mouth.addEventListener('change', function () {
+  imageObj.src = this.options[this.selectedIndex].dataset.img;
+  imageObj.onload = function() {
+    context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+  };
+});
+
+faceitems.addEventListener('change', function () {
+  imageObj.src = this.options[this.selectedIndex].dataset.img;
+  imageObj.onload = function() {
+    context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+  };
+});
+
+glasses.addEventListener('change', function () {
+  imageObj.src = this.options[this.selectedIndex].dataset.img;
+  imageObj.onload = function() {
+    context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+  };
+});
+
+hat.addEventListener('change', function () {
+  imageObj.src = this.options[this.selectedIndex].dataset.img;
+  imageObj.onload = function() {
+    context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+  };
+});
+
+shirt.addEventListener('change', function () {
+  imageObj.src = this.options[this.selectedIndex].dataset.img;
+  imageObj.onload = function() {
+    context.drawImage(imageObj, 0, 0, canvas.width, canvas.height);
+  };
+});
